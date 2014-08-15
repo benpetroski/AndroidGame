fc206456d95b952b511b87ae69c604d35f71a03d
Ben Petroski <ben@benpetroski.com>
2014-08-14 17:02:30 -0700

    Need to finish androidmanifest.xml for game to be done.

--- src/com/benpetroski/bnptrskFirstAndroidGame/GameScreen.java
+++ src/com/benpetroski/bnptrskFirstAndroidGame/GameScreen.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 
 import com.benpetroski.framework.Game;
+import com.benpetroski.framework.Graphics;
 import com.benpetroski.framework.Input.TouchEvent;
 import com.benpetroski.framework.Screen;
 
@@ -34,14 +35,13 @@ public class GameScreen extends Screen {
 
 	@Override
 	public void update(float deltaTime) {
-		List<TouchEvent> touchEvents = game.getInput().getTouchEvents();
-		
+		List<TouchEvent> touchEvents = game.getInput().getTouchEvents();		
 		
 		//Four update methods that update based on game state
 		if (state == GameState.Ready)
 			updateReady(touchEvents);
 		if (state == GameState.Running)
-			updateRunning(touchEvents);
+			updateRunning(touchEvents, deltaTime);
 		if (state == GameState.Paused)
 			updatePaused(touchEvents);
 		if (state == GameState.GameOver)
@@ -49,49 +49,134 @@ public class GameScreen extends Screen {
 	}
 
 	private void updateReady(List<TouchEvent> touchEvents) {
-		
+		//Starts with ready screen, game begins after screen touch
+		if (touchEvents.size() > 0) {
+			state = GameState.Running;
+		}
 	}
 
-	private void updateRunning(List<TouchEvent> touchEvents) {
+	private void updateRunning(List<TouchEvent> touchEvents, float deltaTime) {
+		//Identical to update() method from units 2/3
 		
+		//handle touch events
+		int len = touchEvents.size();
+		for (int i=0; i<len; i++) {
+			TouchEvent event = touchEvents.get(i);
+			
+			if (event.type == TouchEvent.TOUCH_DOWN) {
+				if (event.x < 640) {
+					//move left
+				} else if (event.x > 640) {
+					//move right
+				}
+			}
+			
+			if (event.type == TouchEvent.TOUCH_UP) {
+				if (event.x < 640) {
+					//stop left
+				} else if (event.x > 640) {
+					//stop right
+				}
+			}
+		}
+		
+		//handle misc. events
+		if (livesLeft == 0) {
+			state = GameState.GameOver;
+		}
+		
+		//call indiv. updates here, this is where game updates happen (robot.update();)
 	}
 
 	private void updatePaused(List<TouchEvent> touchEvents) {
-		
+		int len = touchEvents.size();
+		for (int i=0; i<len; i++) {
+			TouchEvent event = touchEvents.get(i);
+			if (event.type == TouchEvent.TOUCH_UP) {
+				
+			}				
+		}
 	}
 
 	private void updateGameOver(List<TouchEvent> touchEvents) {
+		int len = touchEvents.size();
+		for (int i=0; i<len; i++) {
+			TouchEvent event = touchEvents.get(i);
+			if (event.type == TouchEvent.TOUCH_UP) {
+				if (event.x > 300 && event.x < 980 && event.y > 100 && event.y < 500) {
+					nullify();
+					game.setScreen(new MainMenuScreen(game));
+					return;
+				}
+			}
+		}
+	}
+
+	private void nullify() {
+		//set all variables to null and recreate with constructor
+		paint = null;
 		
+		//garbage collection
+		System.gc();
 	}
 
 	@Override
 	public void paint(float deltaTime) {
-		// TODO Auto-generated method stub
+		Graphics g = game.getGraphics();
+		//Draw game elements: g.drawImage(Assets.background, 0, 0);
+		
+		//Draw UI above game elements
+		if (state == GameState.Ready)
+			drawReadyUI();
+		if (state == GameState.Running)
+			drawRunningUI();
+		if (state == GameState.Paused)
+			drawPausedUI();
+		if (state == GameState.GameOver)
+			drawGameOverUI();
+	}
 
+	private void drawReadyUI() {
+		Graphics g = game.getGraphics();
+		
+		g.drawARGB(155, 0, 0, 0);
+		g.drawString("Tap each side of the screen to move in that direction.", 640, 300, paint);
+	}
+
+	private void drawRunningUI() {
+		Graphics g = game.getGraphics();		
+	}
+
+	private void drawPausedUI() {
+		Graphics g = game.getGraphics();
+		
+		//darken screen to display paused menu
+		g.drawARGB(155, 0, 0, 0);
+	}
+
+	private void drawGameOverUI() {
+		Graphics g = game.getGraphics();
+		g.drawRect(0, 0, 1281, 801, Color.BLACK);
+		g.drawString("Game Over.", 640, 300, paint);
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-
+		if (state == GameState.Running)
+			state = GameState.Paused;
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void backButton() {
-		// TODO Auto-generated method stub
-
+		pause();
 	}
 
 }
